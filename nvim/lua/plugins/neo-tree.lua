b5ab7b2f9ac79fe256dbc8b4036781e9b1e42527
yuyudhan <ankurkumarpandey@gmail.com>
2025-01-08 13:53:10 +0530

    nvim, show hidden files by default. aerospace spacing on all sides, tmux scroll ines

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+-- lua/plugins/neo-tree.lua
+
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true, -- Show hidden files by default
+                hide_dotfiles = false, -- Do not hide dotfiles (files starting with .)
+                hide_gitignored = false, -- Optionally show git-ignored files
+            },
+        },
+    },
+}
+
