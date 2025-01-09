c669c2445c5a864fbfdbe452d6dba7c1c99e5b33
Ankur Pandey <ankur@signzy.com>
2025-01-09 16:17:53 +0530

    Add number functoinality for starting and end

--- nvim/lua/plugins/helpers.lua
+++ nvim/lua/plugins/helpers.lua
@@ -2,14 +2,44 @@
 
 local M = {}
 
--- Similar to insertnums functionality of Sublime Text
-vim.api.nvim_create_user_command("AddNumber", function(opts)
+-- Function to add numbers at the start of each line
+vim.api.nvim_create_user_command("AddNumberStart", function(opts)
+    local start_line = opts.line1
+    local end_line = opts.line2
+    local i = 1
+    for lnum = start_line, end_line do
+        local line = vim.fn.getline(lnum)
+        vim.fn.setline(lnum, i .. " " .. line)
+        i = i + 1
+    end
+end, { range = true })
+
+-- Function to add numbers at the end of each line
+vim.api.nvim_create_user_command("AddNumberEnd", function(opts)
     local start_line = opts.line1
     local end_line = opts.line2
     local i = 1
     for lnum = start_line, end_line do
         local line = vim.fn.getline(lnum)
         vim.fn.setline(lnum, line .. " " .. i)
+        i = i + 1
+    end
+end, { range = true })
+
+-- Function to add numbers at each cursor position
+vim.api.nvim_create_user_command("AddNumberCursor", function()
+    -- Get the start and end lines of the Visual selection
+    local start_line = vim.fn.getpos("'<")[2]
+    local end_line = vim.fn.getpos("'>")[2]
+    local start_col = vim.fn.getpos("'<")[3]
+    local i = 1
+
+    -- Iterate over each line in the Visual Block
+    for line_num = start_line, end_line do
+        local line = vim.fn.getline(line_num)
+        -- Add the number at the cursor's column position
+        local new_line = line:sub(1, start_col - 1) .. i .. line:sub(start_col)
+        vim.fn.setline(line_num, new_line)
         i = i + 1
     end
 end, { range = true })
