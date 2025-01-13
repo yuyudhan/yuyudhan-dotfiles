be136cc7cb0e5f87035620f94a130f4b40a1f39f
yuyudhan <ankurkumarpandey@gmail.com>
2025-01-13 21:56:36 +0530

    Added multi cursor for selection

---
+++ nvim/lua/plugins/multiple-cursors.lua
@@ -0,0 +1,17 @@
+-- lua/plugins/multiple-cursors.lua
+
+return {
+    "terryma/vim-multiple-cursors",
+    event = "VeryLazy",
+    config = function()
+        vim.g.multi_cursor_start_word_key = "<C-n>"
+        vim.g.multi_cursor_select_all_word_key = "<C-a>"
+        vim.g.multi_cursor_start_key = "g<C-n>"
+        vim.g.multi_cursor_select_all_key = "g<C-a>"
+        vim.g.multi_cursor_next_key = "<C-n>"
+        vim.g.multi_cursor_prev_key = "<C-p>"
+        vim.g.multi_cursor_skip_key = "<C-x>"
+        vim.g.multi_cursor_quit_key = "<Esc>"
+    end,
+}
+
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,25 +6,42 @@ local wezterm = require("wezterm")
 -- Configuration builder for structured settings
 local config = wezterm.config_builder()
 
+-- ================================
+-- Appearance Settings
+-- ================================
+
 -- Color scheme
--- config.color_scheme = "Batman"
 config.color_scheme = "Chalk (dark) (terminal.sexy)"
 
 -- Font settings
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 14
 
--- Tab bar settings
-config.enable_tab_bar = true
-config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
-
 -- Window appearance
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 25
 
--- Window layout and padding
+-- Window border settings
+config.window_frame = {
+	-- Title bar colors
+	active_titlebar_bg = "#1D1F21", -- Active window title bar color
+	inactive_titlebar_bg = "#282A2E", -- Inactive window title bar color
+
+	-- Border widths
+	border_left_width = 2,
+	border_right_width = 3,
+	border_top_height = 2,
+	border_bottom_height = 2,
+
+	-- Border colors
+	border_left_color = "#44475A",
+	border_right_color = "#44475A",
+	border_top_color = "#44475A",
+	border_bottom_color = "#44475A",
+}
+
+-- Window padding
 config.line_height = 1.2
 config.window_padding = {
 	left = 0,
@@ -33,9 +50,34 @@ config.window_padding = {
 	bottom = 0,
 }
 
--- Vim-like keybindings
+-- ================================
+-- Tab Bar Settings
+-- ================================
+
+config.enable_tab_bar = true
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+
+-- ================================
+-- Key Bindings
+-- ================================
+
 config.keys = {
-	-- Pane navigation
+	-- Clear Scrollback and Screen with Cmd + K (Mac)
+	{
+		key = "K",
+		mods = "CMD",
+		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+	},
+
+	-- Clear Scrollback and Screen with Ctrl + L (Linux/Windows/Mac)
+	{
+		key = "L",
+		mods = "CTRL",
+		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+	},
+
+	-- Pane navigation (Vim-like)
 	{ key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
 	{ key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
 	{ key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
@@ -52,8 +94,7 @@ config.keys = {
 	{ key = "-", mods = "CTRL", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
 	-- Pane closing
-	-- { key = "w", mods = "CTRL", action = wezterm.action.CloseCurrentPane { confirm = true } }, -- Added Ctrl+w
-	{ key = "X", mods = "CTRL", action = wezterm.action.CloseCurrentPane({ confirm = true }) }, -- Added Ctrl+w
+	{ key = "X", mods = "CTRL", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
 }
 
 -- Return the finalized configuration
