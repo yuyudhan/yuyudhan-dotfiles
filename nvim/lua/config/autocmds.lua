5375e696bc807b42e5af1e1dc947fb44a85703d8
Ankur Pandey <ankur@signzy.com>
2025-03-18 18:30:42 +0530

    Nvim remove double blank lines, some formatting.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -53,3 +53,22 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end,
 })
 
+-- Remove multiple consecutive blank lines
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        -- Save cursor position
+        local cursor_pos = vim.api.nvim_win_get_cursor(0)
+
+        -- Replace multiple blank lines with a single blank line
+        vim.cmd([[%s/\n\n\n\+/\r\r/e]])
+
+        -- Restore cursor position (adjusting if needed)
+        local new_last_line = vim.api.nvim_buf_line_count(0)
+        if cursor_pos[1] > new_last_line then
+            cursor_pos[1] = new_last_line
+        end
+        vim.api.nvim_win_set_cursor(0, cursor_pos)
+    end,
+})
+
